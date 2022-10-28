d0272c38aa5a2878394a59e24d35702bca58b71c
Adam Gallina <adamg461@gmail.com>
2022-10-27 20:24:12 -0700

    Mirror is stupid

--- Assets/Scripts/Actors/LocalPlayer.cs
+++ Assets/Scripts/Actors/LocalPlayer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : PlaneController
+public class LocalPlayer : PlaneController
 {
     private bool allowMovement = false;
 
